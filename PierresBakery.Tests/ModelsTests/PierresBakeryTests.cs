7396aa03ab5072b5810a4ad37b479d8be995d079
Jonathan Lu <jdlu.coding@gmail.com>
2022-07-15 14:17:04 -0700

    TDD: create one instance for each class and check type to equal to a number

--- PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
+++ PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
@@ -5,6 +5,26 @@ using PierresBakery.Models;
 
 namespace PierresBakery.Tests
 {
- 
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreateInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+        [TestMethod]
+    public void PastryConstructor_CreateInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
+
+
+
+  }
   
 }
