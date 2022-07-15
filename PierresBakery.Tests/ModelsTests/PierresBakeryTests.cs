de91bb253e2beb30e6279c393faff394940cbf71
Jonathan Lu <jdlu.coding@gmail.com>
2022-07-15 10:55:14 -0700

    complete initial project setup

---
+++ PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+ 
+  
+}
---
+++ PierresBakery/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresBakery
+{
+  public class Program
+  {
+    public static void Main() 
+    {
+
+    }
+  }
+}
