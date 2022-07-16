106f21c7c43afa2d8e8a33a186bde08c92a0956e
Jonathan Lu <jdlu.coding@gmail.com>
2022-07-15 17:01:56 -0700

    WIP & Debug: encountered error 'not all code paths return a value'. Still trying to figure out

--- PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
+++ PierresBakery.Tests/ModelsTests/PierresBakeryTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
 using PierresBakery.Models;
 
 namespace PierresBakery.Tests
@@ -12,6 +12,7 @@ namespace PierresBakery.Tests
     public void BreadConstructor_CreateInstanceOfBread_Bread()
     {
       Bread newBread = new Bread(1);
+
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
@@ -19,6 +20,7 @@ namespace PierresBakery.Tests
     public void PastryConstructor_CreateInstanceOfPastry_Pastry()
     {
       Pastry newPastry = new Pastry(1);
+
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
 
@@ -26,8 +28,10 @@ namespace PierresBakery.Tests
     public void GetQuantity_ReturnQuantityOfBread_Integer()
     {
       int quantity = 2;
+
       Bread newBread = new Bread(quantity);
       int result = newBread.Quantity;
+
       Assert.AreEqual(quantity, result);
     }
 
@@ -35,8 +39,10 @@ namespace PierresBakery.Tests
     public void GetQuantity_ReturnQuantityOfPastry_Integer()
     {
       int quantity = 2;
+
       Pastry newPastry = new Pastry(quantity);
       int result = newPastry.Quantity;
+
       Assert.AreEqual(quantity, result);
     }
 
@@ -48,6 +54,7 @@ namespace PierresBakery.Tests
       int updatedQuantity = 5;
       newBread.Quantity = updatedQuantity;
       int result = newBread.Quantity;
+
       Assert.AreEqual(updatedQuantity, result);
     }
 
@@ -59,8 +66,20 @@ namespace PierresBakery.Tests
       int updatedQuantity = 5;
       newPastry.Quantity = updatedQuantity;
       int result = newPastry.Quantity;
+
       Assert.AreEqual(updatedQuantity, result);
-    }    
+    }
+
+    [TestMethod]
+    public void PastryCost_CostOfOnePastry_Integer()
+    {
+      int pastryQuantity = 1;
+      double pastryCost = 2;
+
+      Pastry pastryOrder = new Pastry(pastryQuantity);
+
+      Assert.AreEqual(pastryCost, pastryOrder.PastryCost());
+    }
 
 
   }
